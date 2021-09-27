e0173506355834cad0ac0a9a925d59475edba4a3
govind6498 <govindsingh6498@gmail.com>
2021-09-27 21:44:52 +0530

    Added:To initialize project Structure

---
+++ src/main/java/com/bridgelabz/employeepayrolljdbc/EmployeePayrollService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeepayrolljdbc;
+
+public class EmployeePayrollService {
+	public static void main(String[] args) {
+		System.out.println("Display to JDBC program");
+	}
+}
---
+++ src/test/java/com/bridgelabz/employeepayrolljdbc/EmployeePayrollTest.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.employeepayrolljdbc;
+
+public class EmployeePayrollTest {
+
+}
