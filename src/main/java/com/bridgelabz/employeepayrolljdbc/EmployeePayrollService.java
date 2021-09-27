66120cca1270ac1ca901559aea859c110c4bfd0f
govind6498 <govindsingh6498@gmail.com>
2021-09-27 21:49:06 +0530

    Added:To connect JDBC

--- src/main/java/com/bridgelabz/employeepayrolljdbc/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrolljdbc/EmployeePayrollService.java
@@ -1,7 +1,41 @@
 package com.bridgelabz.employeepayrolljdbc;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
 public class EmployeePayrollService {
 	public static void main(String[] args) {
-		System.out.println("Display to JDBC program");
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+		String userName = "root";
+		String password = "bridgelabz@123";
+		Connection connection;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver Loaded");
+		}
+		catch(ClassNotFoundException e) {
+			throw new IllegalStateException("Can not find the driver in the classpath!",e);
+		}
+
+		listDriver();
+		try {
+			System.out.println("Connecting to database:"+jdbcURL);
+			connection = DriverManager.getConnection(jdbcURL,userName,password);
+			System.out.println("Connection is successfull!!!!"+connection);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	private static void listDriver() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while(driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println("  "+driverClass.getClass().getName());
+		}
 	}
 }
